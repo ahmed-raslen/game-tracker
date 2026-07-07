ff984387f8428b13e9626a02c637f1de5a9ca154
ahmed-raslen <ahmedraslen666@gmail.com>
2026-07-07 17:02:25 +0100

    Configure PostgreSQL connection and add gitignore

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'game_tracker',
+        'USER': 'postgres',
+        'PASSWORD': 'ehaz//2020',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
